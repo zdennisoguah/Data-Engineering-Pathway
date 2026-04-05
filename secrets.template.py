6868d581aa78c0a8aed5aac91d62e87b053bbcdb
Dennis Oguah <zdennisoguah@gmail.com>
2026-04-05 13:46:24 +0100

    fix: correct nested file structure and add secrets template

--- secrets.template.py
+++ secrets.template.py
@@ -1,4 +1,4 @@
 
-STORAGE_ACCOUNT_NAME = "delakehousezdennis"
-STORAGE_ACCOUNT_KEY  = "7WOg7asDRnv+nyjfZM3Qe8QKltM7x65vuL+Vytd8mZ8nh39C0XDUGjSu+lCGSg/aN0t4RUR9JTkY+AStwRqZ7w=="
-EVENTHUB_CONNECTION  = "Endpoint=sb://de-eventhub-zdennis.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9xEe/BQoIbNUHvIXh9vjP4fZIU9RfFNwK+AEhD8Y64w="
+STORAGE_ACCOUNT_NAME = "your_storage_account_name_here"  # from Azure Portal → Storage Accounts → Access keys
+STORAGE_ACCOUNT_KEY  = "your_adls_key_from_azure_portal"
+EVENTHUB_CONNECTION  = "your_event_hubs_connection_string"  # from Azure Portal → Event Hubs → Shared access policies
